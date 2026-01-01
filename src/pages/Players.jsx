dc182513b45dbae85869593d61b7980493f4625b
Pratik Rane <pratikrane0412@gmail.com>
2026-01-01 19:37:59 +0530

    Configure for production v2

--- src/pages/Players.jsx
+++ src/pages/Players.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
-import "../styles/players.css";
+import "../styles/Players.css";
 
 export default function Players() {
   const [players, setPlayers] = useState([]);
--- src/pages/Positions.jsx
+++ src/pages/Positions.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import '../styles/positions.css';
+import '../styles/Positions.css';
 
 export default function Positions() {
     const [positions, setPositions] = useState([]);
